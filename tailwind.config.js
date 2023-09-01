bff824f40f6714f8f8ba9b09802c8d7f6bc4e42a
Cathryn Ngaosi <cathrynngaosi@gmail.com>
2023-09-01 21:13:48 +0800

    Add custom css, import second logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Raleway", "sans-serif"],
+      },
+    },
+    colors: {
+      seashellPink: "#F4E7E1",
+      seashellNude: "##F6EDE9",
+      mediumBrown: "#966147",
+      lightBrown: "#C7AC9E",
+    },
   },
   plugins: [],
 };
