d0d5c4840d437db78585dcbf3200e5612c5c754b
Cathryn Ngaosi <cathrynngaosi@gmail.com>
2023-09-03 19:17:43 +0800

    Create function to get products based on a query

--- src/services/apiShop.js
+++ src/services/apiShop.js
@@ -1,9 +1,23 @@
+import { formatTitle } from "../utils/helpers";
+
 const API_URL = "http://localhost:8000";
 
-export async function getCollections() {
+export async function getAllCollections() {
   const res = await fetch(`${API_URL}/collections`);
 
   if (!res.ok) throw Error("Failed getting collections");
+
+  const data = await res.json();
+
+  return data;
+}
+
+export async function getProducts(query) {
+  const res = await fetch(
+    `${API_URL}/products?q=${query === "shop-all" ? "" : formatTitle(query)}`
+  );
+
+  if (!res.ok) throw Error("Failed getting products");
 
   const data = await res.json();
 
