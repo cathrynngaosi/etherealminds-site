ec80a87aaa74feb1d7e251c91029920858c4239b
Cathryn Ngaosi <cathrynngaosi@gmail.com>
2023-09-05 14:26:16 +0800

    Add ability to generate preview

--- src/pages/ProductView.jsx
+++ src/pages/ProductView.jsx
@@ -1,14 +1,48 @@
 import { useLoaderData } from "react-router-dom";
 import { getProducts } from "../services/apiShop";
-import ImagePreview from "../features/shop/ImagePreview";
-import { formatCurrency } from "../utils/helpers";
-import ColorBlock from "../features/shop/ColorBlock";
+import { formatCurrency, getCustomColorCss } from "../utils/helpers";
+import ColorBlock from "../features/products/ColorBlock";
 import { FiHeart } from "react-icons/fi";
 import { useEffect, useState } from "react";
+import ImagePreview from "../features/products/ImagePreview";
 
 function ProductView() {
   const productDetails = useLoaderData();
   const { name, price, images, colors, description } = productDetails[0];
+
+  const [customStyle, setCustomStyle] = useState(
+    "focus:outline-none text-xl border font-anton rounded px-4 py-4 md:w-[80%] w-full border-seashellNude cursor-default"
+  );
+  const [customText, setCustomText] = useState("");
+  const [customFontCss, setCustomFontCss] = useState("font-anton");
+  const [customFontDisplay, setCustomFontDisplay] = useState("Anton");
+  const [customColorCss, setCustomColorCss] = useState("black");
+  const [customColorDisplay, setCustomColorDisplay] = useState("Black");
+  const [productColor, setProductColor] = useState(colors[0]);
+
+  useEffect(() => {
+    async function generatePreview() {
+      const initialStyle =
+        "focus:outline-none text-2xl border rounded px-4 py-4 md:w-[80%] w-full border-seashellNude cursor-default";
+      setCustomStyle(`${initialStyle} ${customFontCss}  ${customColorCss}`);
+    }
+
+    generatePreview();
+  }, [customFontCss, customColorCss]);
+
+  function handleClickProdColor(color) {
+    setProductColor(color);
+  }
+
+  function handleClickCustomColor(color) {
+    setCustomColorCss(getCustomColorCss(color));
+    setCustomColorDisplay(color);
+  }
+
+  function handleClickCustomFont(font) {
+    setCustomFontCss(font);
+    setCustomFontDisplay(font.replace("font-", ""));
+  }
 
   return (
     <section className="flex py-10 md:px-14 px-10 space-y-4 md:space-y-0 md:space-x-4 flex-col md:flex-row">
@@ -33,12 +67,19 @@ function ProductView() {
 
         {colors.length > 0 && (
           <div className="space-y-1">
-            <h2 className="uppercase text-lg font-semibold text-darkBrown">
-              Product Color:
-            </h2>
+            <div className="flex items-center space-x-1 text-darkBrown">
+              <h2 className="uppercase text-lg font-semibold ">
+                Product Color:{" "}
+              </h2>
+              <span className="font-medium">{productColor}</span>
+            </div>
             <div className="flex space-x-1">
               {colors.map((color) => (
-                <ColorBlock color={color} key={color} />
+                <ColorBlock
+                  color={color}
+                  key={color}
+                  handleClick={() => handleClickProdColor(color)}
+                />
               ))}
             </div>
           </div>
@@ -50,40 +91,75 @@ function ProductView() {
           </h2>
           <input
             type="text"
+            value={customText}
+            onChange={(e) => setCustomText(e.target.value)}
             className="border border-lightBrown w-full md:w-[80%] focus:outline-none py-1 rounded px-2"
           />
         </div>
         <div className="flex flex-col md:flex-row space-y-4 md:space-y-0">
           <div className="space-y-1 w-1/2">
-            <h2 className="uppercase text-lg font-semibold text-darkBrown">
-              Font:
-            </h2>
+            <div className="flex items-center space-x-1 text-darkBrown">
+              <h2 className="uppercase text-lg font-semibold ">Font:</h2>
+              <span className="font-medium capitalize">
+                {customFontDisplay}
+              </span>
+            </div>
+
             <div className="space-x-1 flex">
-              <div className="px-4 py-[1px] text-md items-center flex border cursor-pointer font-anton">
+              <div
+                className="px-4 py-[1px] text-md items-center flex border cursor-pointer font-anton"
+                onClick={() => handleClickCustomFont("font-anton")}
+              >
                 <span>Anton</span>
               </div>
-              <div className="px-4 py-[1px] text-xl items-center flex border cursor-pointer font-cookie">
+              <div
+                className="px-4 py-[1px] text-xl items-center flex border cursor-pointer font-cookie"
+                onClick={() => handleClickCustomFont("font-cookie")}
+              >
                 <span> Cookie</span>
               </div>
-              <div className="px-4 py-[1px] text-xl items-center flex border cursor-pointer font-tenali">
+              <div
+                className="px-4 py-[1px] text-xl items-center flex border cursor-pointer font-tenali"
+                onClick={() => handleClickCustomFont("font-tenali")}
+              >
                 <span className="mt-[5px]"> Tenali</span>
               </div>
-              <div className="px-4 py-[1px] text-sm items-center flex border cursor-pointer font-pacifico">
+              <div
+                className="px-4 py-[1px] text-sm items-center flex border cursor-pointer font-pacifico"
+                onClick={() => handleClickCustomFont("font-pacifico")}
+              >
                 <span> Pacifico</span>
               </div>
             </div>
           </div>
 
           <div className="space-y-1 w-1/2">
-            <h2 className="uppercase text-lg font-semibold text-darkBrown">
-              Decal Color:
-            </h2>
+            <div className="flex items-center space-x-1 text-darkBrown">
+              <h2 className="uppercase text-lg font-semibold ">Decal Color:</h2>
+              <span className="font-medium">{customColorDisplay}</span>
+            </div>
+
             <div className="flex space-x-1">
-              <div className="h-9 w-9 bg-white border border-slate-10	cursor-pointer"></div>
-              <div className="h-9 w-9 bg-black border border-slate-10	cursor-pointer"></div>
-              <div className="h-9 w-9 bg-gold border border-slate-10	cursor-pointer"></div>
-              <div className="h-9 w-9 bg-rosegold border border-slate-10	cursor-pointer"></div>
-              <div className="h-9 w-9 bg-gradient-to-b from-cyan-200 to-rose-200 border border-slate-10	cursor-pointer"></div>
+              <div
+                className="h-9 w-9 bg-black border border-slate-10 cursor-pointer"
+                onClick={() => handleClickCustomColor("Black")}
+              ></div>
+              <div
+                className="h-9 w-9 bg-white border border-slate-10	cursor-pointer"
+                onClick={() => handleClickCustomColor("White")}
+              ></div>
+              <div
+                className="h-9 w-9 bg-gold border border-slate-10	cursor-pointer"
+                onClick={() => handleClickCustomColor("Gold")}
+              ></div>
+              <div
+                className="h-9 w-9 bg-rosegold border border-slate-10	cursor-pointer"
+                onClick={() => handleClickCustomColor("Rosegold")}
+              ></div>
+              <div
+                className="h-9 w-9 bg-gradient-to-b from-cyan-400 to-rose-400 border border-slate-10	cursor-pointer"
+                onClick={() => handleClickCustomColor("Holo")}
+              ></div>
             </div>
           </div>
         </div>
@@ -92,7 +168,7 @@ function ProductView() {
           <h2 className="uppercase text-lg font-semibold text-darkBrown">
             Preview:
           </h2>
-          <div></div>
+          <div className={customStyle}>{customText}</div>
         </div>
 
         <div className="flex space-x-2 mt-10">
